9dce4dcf4c461a648ff6329218643335f4acf723
yashvarmora2306 <yashpatel23062002@gmail.com>
2023-12-28 21:57:12 +0530

    complate responsive Header

--- src/components/home/header/Header.js
+++ src/components/home/header/Header.js
@@ -34,7 +34,7 @@ const Header = () => {
                 <Flex className="flex items-center justify-between h-full">
                     <Link to="/">
                         <div>
-                            <Image className="w-30 object-cover" imgSrc={logo} />
+                            <Image className="w-30 h-10 object-cover" imgSrc={logo} />
                         </div>
                     </Link>
                     <div>
